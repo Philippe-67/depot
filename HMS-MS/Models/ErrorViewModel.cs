a634411a4bf03b330091f515be8eb67f17b007f4
Philippe CHARLES <A114SR@login.axa>
2023-12-29 19:33:41 +0100

    ajout dossier data migrations repositories

---
+++ HMS-MS/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace HMS_MS.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
