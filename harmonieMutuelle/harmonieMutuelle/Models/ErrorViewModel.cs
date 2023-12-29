94b4da1e8c7ddcdd61ee953426ba609be082f079
Philippe CHARLES <A114SR@login.axa>
2023-12-29 18:44:22 +0100

    positionnement du FrontEnd dans Projet Hoarmonie Mutuelle

---
+++ harmonieMutuelle/harmonieMutuelle/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace harmonieMutuelle.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
